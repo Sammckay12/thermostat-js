ba48783d82f480d1db6fff5748715d846d5ca65c
Sam Mckay <sammckay12@gmail.com>
2017-02-02 12:57:46 +0000

    min temperature is 10

--- jasmine-standalone-2.5.2/spec/ThermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -9,6 +9,9 @@ describe('thermostat', function() {
     it('starts on 20 degrees' , function() {
       expect(thermostat.degrees()).toEqual(20);
     });
+    it('The minimum temperature is 10 degrees', function(){
+      expect(function() {thermostat.degrees(9)}).toThrow('Cannot change temperature: Min temperature is 10')
+    });
   });
 
   describe('up(degrees)', function() {
@@ -16,6 +19,15 @@ describe('thermostat', function() {
       thermostat.up(5);
       expect(thermostat.degrees()).toEqual(25);
     });
+  });
+
+  describe('down(degrees)', function() {
+    it('decreases temperature by degrees' , function() {
+      thermostat.down(5);
+      expect(thermostat.degrees()).toEqual(15);
+    });
+
+
   });
 
 
--- jasmine-standalone-2.5.2/src/Thermostat.js
+++ jasmine-standalone-2.5.2/src/Thermostat.js
@@ -1,11 +1,17 @@
 function Thermostat () {
   this._degrees = 20;
+  this._minTemperature = 10;
 };
 
 Thermostat.prototype.degrees = function(degrees) {
+  if (degrees < this._minTemperature) { throw ('Cannot change temperature: Min temperature is ' + this._minTemperature ) };
   return this._degrees = degrees || this._degrees
 };
 
 Thermostat.prototype.up = function(degrees) {
   return this.degrees(this.degrees() + degrees)
+};
+
+Thermostat.prototype.down = function(degrees) {
+  return this.degrees(this.degrees() - degrees)
 };
