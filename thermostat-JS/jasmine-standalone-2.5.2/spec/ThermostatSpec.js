2e1bd140f4e4af863f07912ca7878101e016e464
Sam Mckay <sammckay12@gmail.com>
2017-02-02 12:12:25 +0000

    starts at 20 degrees

---
+++ thermostat-JS/jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -0,0 +1,20 @@
+describe('thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+describe('degrees', function() {
+  it('starts on 20 degrees' , function() {
+    expect(thermostat.degrees()).toEqual(20);
+  });
+});
+
+
+
+
+
+
+
+ });
---
+++ thermostat-JS/jasmine-standalone-2.5.2/src/Thermostat.js
@@ -0,0 +1,13 @@
+function Thermostat () {
+  this._degrees = 20
+};
+
+// Thermostat.prototype = function() {
+//   this.degrees = function() {return this._degrees}
+//
+// };
+
+  Thermostat.prototype.degrees = function() {
+    return this._degrees
+
+  };
