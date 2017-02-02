e8f4cfa8af05b32e8425a694b1a96dcdd6580c6c
Sam Mckay <sammckay12@gmail.com>
2017-02-02 16:26:05 +0000

    refactored

--- jasmine-standalone-2.5.2/spec/ThermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -5,29 +5,37 @@ describe('thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('degrees', function() {
+  describe('readTemperature()', function() {
     it('starts on 20 degrees' , function() {
-      expect(thermostat.degrees()).toEqual(20);
+      expect(thermostat.readTemperature()).toEqual(20);
     });
+  });
+
+  describe('changeTemperature(degrees)', function(){
     it('The minimum temperature is 10 degrees', function(){
-      expect(function() {thermostat.degrees(9)}).toThrow('Cannot change temperature: Min temperature is 10')
+      expect(function() {thermostat.changeTemperature(9)}).toThrow('Cannot change temperature: Min temperature is 10')
     });
   });
 
   describe('up(degrees)', function() {
     it('increases temperature by degrees' , function() {
       thermostat.up(5);
-      expect(thermostat.degrees()).toEqual(25);
+      expect(thermostat.readTemperature()).toEqual(25);
     });
   });
 
   describe('down(degrees)', function() {
     it('decreases temperature by degrees' , function() {
       thermostat.down(5);
-      expect(thermostat.degrees()).toEqual(15);
+      expect(thermostat.readTemperature()).toEqual(15);
     });
+  });
 
-
+  describe('power saving', function() {
+    it('has a maximum temperature of 25 when power saving is on' , function() {
+      // thermostat.up(5);
+      // expect(thermostat.degrees()).toEqual(25);
+    });
   });
 
 
--- jasmine-standalone-2.5.2/src/Thermostat.js
+++ jasmine-standalone-2.5.2/src/Thermostat.js
@@ -3,15 +3,19 @@ function Thermostat () {
   this._minTemperature = 10;
 };
 
-Thermostat.prototype.degrees = function(degrees) {
+Thermostat.prototype.readTemperature = function(){
+    return this._degrees
+ };
+
+Thermostat.prototype.changeTemperature = function(degrees) {
   if (degrees < this._minTemperature) { throw ('Cannot change temperature: Min temperature is ' + this._minTemperature ) };
-  return this._degrees = degrees || this._degrees
+  return this._degrees = degrees
 };
 
 Thermostat.prototype.up = function(degrees) {
-  return this.degrees(this.degrees() + degrees)
+  return this.changeTemperature(degrees + this.readTemperature())
 };
 
 Thermostat.prototype.down = function(degrees) {
-  return this.degrees(this.degrees() - degrees)
+  return this.changeTemperature( this.readTemperature() - degrees)
 };
